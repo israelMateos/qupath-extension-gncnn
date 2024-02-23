0a7a4bba6c8dc45815bee381dbf9c562cf20d695
Israel Mateos Aparicio <israel4720@gmail.com>
2024-02-23 10:01:10 +0100

    Added single image tiling and utils

--- src/main/java/qupath/ext/gdcnn/TilerTask.java
+++ src/main/java/qupath/ext/gdcnn/TilerTask.java
@@ -5,8 +5,6 @@ import org.slf4j.LoggerFactory;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.nio.file.Paths;
-import java.nio.file.Files;
 import javafx.concurrent.Task;
 
 import java.util.List;
@@ -49,35 +47,31 @@ public class TilerTask extends Task<Void> {
     @Override
     protected Void call() throws Exception {
         Project<BufferedImage> project = qupath.getProject();
-        List<ProjectImageEntry<BufferedImage>> imageEntryList = project.getImageList();
-        int nImages = imageEntryList.size();
-        int i = 1;
-
-        logger.info("Tiling {} images in the project to patches of size {}", nImages, tileSize);
-        // For each image, tile it and save the tiles in a temporary folder
-        for (ProjectImageEntry<BufferedImage> imageEntry : imageEntryList) {
-            ImageData<BufferedImage> imageData = imageEntry.readImageData();
-            logger.info("Tiling image {}/{} : {}", i, nImages, imageData.getServer().getMetadata().getName());
-            String outputPath = GeneralTools.stripExtension(imageData.getServer().getMetadata().getName()) + "_tiles";
-            // Create the output folder if it does not exist
-            Files.createDirectories(Paths.get(outputPath));
-            tileWSI(imageData, outputPath);
-            logger.info("Finished tiling image {}/{} : {}", i, nImages, outputPath);
-            i++;
+        if (project != null) {
+            tileWSIProject(project);
+        } else {
+            ImageData<BufferedImage> imageData = qupath.getImageData();
+            if (imageData != null) {
+                tileWSI(imageData);
+            } else {
+                logger.error("No image or project is open");
+            }
         }
-
-        logger.info("Tiling images in the project finished");
         return null;
     }
 
     /**
-     * Tiles the image data and saves the tiles in the given output path
+     * Tiles the image data and saves the tiles
      * 
      * @param imageData
-     * @param outputPath
      * @throws IOException
      */
-    private void tileWSI(ImageData<BufferedImage> imageData, String outputPath) throws IOException {
+    private void tileWSI(ImageData<BufferedImage> imageData) throws IOException {
+        String imageName = imageData.getServer().getMetadata().getName();
+        String outputPath = GeneralTools.stripExtension(imageName) + "_tiles";
+        // Create the output folder if it does not exist
+        Utils.createFolder(outputPath);
+        logger.info("Tiling {} [size={},overlap={}]", imageName, tileSize, tileOverlap);
         new TileExporter(imageData)
                 .tileSize(tileSize)
                 .imageExtension(imageExtension)
@@ -85,6 +79,27 @@ public class TilerTask extends Task<Void> {
                 .downsample(downsample)
                 .annotatedTilesOnly(false)
                 .writeTiles(outputPath);
+        logger.info("Tiling of {} finished", imageName);
+    }
+
+    /**
+     * Tiles each WSI in a project and saves them in corresponding temporary folders
+     * 
+     * @param project
+     * @throws IOException
+     */
+    private void tileWSIProject(Project<BufferedImage> project) throws IOException {
+        List<ProjectImageEntry<BufferedImage>> imageEntryList = project.getImageList();
+
+        logger.info("Tiling {} images in the project [size={},overlap={}]",
+                imageEntryList.size(), tileSize, tileOverlap);
+        // For each image, tile it and save the tiles in a temporary folder
+        for (ProjectImageEntry<BufferedImage> imageEntry : imageEntryList) {
+            ImageData<BufferedImage> imageData = imageEntry.readImageData();
+            tileWSI(imageData);
+        }
+
+        logger.info("Tiling images in the project finished");
     }
 
 }
