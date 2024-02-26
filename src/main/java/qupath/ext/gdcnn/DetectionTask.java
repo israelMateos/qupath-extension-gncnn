6546fdf3e9a579861e19c487810f1607a8053d2c
Israel Mateos Aparicio <israel4720@gmail.com>
2024-02-26 19:37:08 +0100

    Added loading of detections for a single image

--- src/main/java/qupath/ext/gdcnn/DetectionTask.java
+++ src/main/java/qupath/ext/gdcnn/DetectionTask.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import javafx.concurrent.Task;
+import java.nio.file.Paths;
 
 import java.util.List;
 import java.util.Arrays;
@@ -14,14 +15,16 @@ import qupath.lib.gui.QuPathGUI;
 import qupath.ext.env.VirtualEnvironment;
 import qupath.lib.common.GeneralTools;
 import qupath.lib.images.ImageData;
+import qupath.lib.io.PathIO;
+import qupath.lib.objects.PathObject;
+import qupath.lib.objects.hierarchy.PathObjectHierarchy;
 import qupath.lib.projects.Project;
 import qupath.lib.projects.ProjectImageEntry;
 
 /**
- * Class to detect glomeruli in the WSI patches, and will return a list of the
- * detected objects
+ * Class to detect glomeruli in the WSI patches, and add the detected objects to
+ * the image hierarchy
  */
-// public class DetectionTask extends Task<List<PathObject>> {
 public class DetectionTask extends Task<Void> {
 
     private static final Logger logger = LoggerFactory.getLogger(DetectionTask.class);
@@ -49,7 +52,6 @@ public class DetectionTask extends Task<Void> {
     }
 
     @Override
-    // protected List<PathObject> call() throws Exception {
     protected Void call() throws Exception {
         Project<BufferedImage> project = qupath.getProject();
         if (project != null) {
@@ -68,16 +70,15 @@ public class DetectionTask extends Task<Void> {
     }
 
     /**
-     * Detects glomeruli in the WSI and returns a list of the detected objects
+     * Detects glomeruli in the WSI and adds the detected objects to the image
+     * hierarchy
      * 
      * @param imageData
-     * @return List of detected objects
      * @throws InterruptedException
      * @throws IOException
      */
-    // public List<PathObject> detectGlomeruli(ImageData<BufferedImage> imageData)
-    // throws IOException, InterruptedException {
-    public void detectGlomeruli(ImageData<BufferedImage> imageData) throws IOException, InterruptedException {
+    public void detectGlomeruli(ImageData<BufferedImage> imageData)
+    throws IOException, InterruptedException {
         String imageName = GeneralTools.stripExtension(imageData.getServer().getMetadata().getName());
         VirtualEnvironment venv = new VirtualEnvironment(this.getClass().getSimpleName(), pythonPath, gdcnnPath);
 
@@ -99,19 +100,26 @@ public class DetectionTask extends Task<Void> {
         logger.info("Running detection for {}", imageName);
         venv.runCommand();
         logger.info("Detection for {} finished", imageName);
+
+        // Read the annotations from the GeoJSON file
+        String geoJSONPath = gdcnnPath + "Temp/segment-output/Detections/" + imageName + "/detections.geojson";
+        List<PathObject> detectedObjects = PathIO.readObjects(Paths.get(geoJSONPath));
+
+        // Add the detected objects to the image hierarchy
+        // FIXME: Working for single image, but not for the project
+        PathObjectHierarchy hierarchy =  imageData.getHierarchy();
+        hierarchy.addObjects(detectedObjects);
+        logger.info("Added {} detected objects to {}", detectedObjects.size(), imageName);
     }
 
     /**
-     * Detects glomeruli in the WSIs in a project and returns a list of the detected
-     * objects
+     * Detects glomeruli in the WSIs in a project and adds the detected objects to
+     * the each image hierarchy
      * 
      * @param project
-     * @return List of detected objects
      * @throws InterruptedException
      * @throws IOException
      */
-    // public List<PathObject> detectGlomeruliProject(Project<BufferedImage>
-    // project) throws IOException, InterruptedException {
     public void detectGlomeruliProject(Project<BufferedImage> project) throws IOException, InterruptedException {
         List<ProjectImageEntry<BufferedImage>> imageEntryList = project.getImageList();
         logger.info("Running detection for {} images", imageEntryList.size());
