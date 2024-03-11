d54c49051773dbb2e2deff7fde266242fc056397
Israel Mateos Aparicio <israel4720@gmail.com>
2024-03-11 12:13:40 +0100

    Added progress and fixed refreshing glomeruli classes

--- src/main/java/qupath/ext/ui/GDCnnController.java
+++ src/main/java/qupath/ext/ui/GDCnnController.java
@@ -3,6 +3,8 @@ package qupath.ext.ui;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.List;
+import java.util.Collection;
+import java.util.LinkedHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -17,6 +19,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressIndicator;
+import javafx.scene.image.ImageView;
 import qupath.ext.gdcnn.AnnotationExportTask;
 import qupath.ext.gdcnn.ClassificationTask;
 import qupath.ext.gdcnn.DetectionTask;
@@ -27,6 +30,9 @@ import qupath.lib.common.GeneralTools;
 import qupath.lib.common.ThreadTools;
 import qupath.lib.gui.QuPathGUI;
 import qupath.lib.images.ImageData;
+import qupath.lib.objects.PathObject;
+import qupath.lib.objects.hierarchy.PathObjectHierarchy;
+import qupath.lib.objects.hierarchy.events.PathObjectSelectionModel;
 import qupath.lib.projects.Project;
 import qupath.lib.projects.ProjectImageEntry;
 
@@ -35,29 +41,40 @@ public class GDCnnController {
     private static final Logger logger = LoggerFactory.getLogger(GDCnnController.class);
 
     private QuPathGUI qupath;
-	
-	@FXML
-	private Button selectAllImgsBtn;
-	@FXML
-	private Button runAllBtn;
-	@FXML
-	private Button runDetectionBtn;
-	@FXML
-	private Button runClassificationBtn;
-	@FXML
-	private ProgressIndicator progressInd;
-	@FXML
-	private Label progressLabel;
-	@FXML
-	private Button viewResultsBtn;
-	@FXML
-	private CheckListView<String> imgsCheckList;
 
-    private final ExecutorService pool = Executors.newSingleThreadExecutor(ThreadTools.createThreadFactory("GDCnn", true));
+    @FXML
+    private Button selectAllImgsBtn;
+    @FXML
+    private Button runAllBtn;
+    @FXML
+    private Button runDetectionBtn;
+    @FXML
+    private Button runClassificationBtn;
+    @FXML
+    private ProgressIndicator progressInd;
+    @FXML
+    private Label progressLabel;
+    @FXML
+    private ImageView tickIconImg;
+    @FXML
+    private Label doneLabel;
+    @FXML
+    private Button viewResultsBtn;
+    @FXML
+    private CheckListView<String> imgsCheckList;
 
-	// TODO: Use the number of tasks to change the progress indicator
-    private final int TOTAL_TASKS = 5;
-    private int completedTasks = 0;
+    private final ExecutorService pool = Executors
+            .newSingleThreadExecutor(ThreadTools.createThreadFactory("GDCnn", true));
+
+    private static final LinkedHashMap<String, String> PROGRESS_MESSAGES = new LinkedHashMap<String, String>() {
+        {
+            put("ThresholdTask", "Detecting tissue...");
+            put("TilerTask", "Tiling images...");
+            put("DetectionTask", "Detecting glomeruli... (this may take a while)");
+            put("AnnotationExportTask", "Exporting glomerular annotations...");
+            put("ClassificationTask", "Classifying glomeruli...");
+        }
+    };
 
     @FXML
     /**
@@ -68,12 +85,12 @@ public class GDCnnController {
 
         this.qupath = QuPathGUI.getInstance();
     }
-	
-	@FXML
+
+    @FXML
     /**
      * Runs the detection and classification of the glomeruli
      */
-	private void runAll() {
+    private void runAll() {
         if (!isImageOrProjectOpen()) {
             Dialogs.showErrorMessage("No image or project open", "Please open an image or project to run the tasks");
             return;
@@ -88,15 +105,15 @@ public class GDCnnController {
             } catch (IOException e) {
                 logger.error("Error running all tasks", e);
                 Dialogs.showErrorMessage("Error running all tasks", e);
-            }	
+            }
         }
-	}
-	
-	@FXML
+    }
+
+    @FXML
     /**
      * Runs the detection of the glomeruli
      */
-	private void runDetection() {
+    private void runDetection() {
         if (!isImageOrProjectOpen()) {
             Dialogs.showErrorMessage("No image or project open", "Please open an image or project to run the tasks");
             return;
@@ -111,13 +128,13 @@ public class GDCnnController {
                 Dialogs.showErrorMessage("Error running detection", e);
             }
         }
-	}
-	
-	@FXML
+    }
+
+    @FXML
     /**
      * Runs the classification of the glomeruli
      */
-	private void runClassification() {
+    private void runClassification() {
         if (!isImageOrProjectOpen()) {
             Dialogs.showErrorMessage("No image or project open", "Please open an image or project to run the tasks");
             return;
@@ -131,13 +148,13 @@ public class GDCnnController {
                 Dialogs.showErrorMessage("Error running classification", e);
             }
         }
-	}
-	
-	@FXML
+    }
+
+    @FXML
     // TODO
-	private void viewResults() {
-		
-	}
+    private void viewResults() {
+
+    }
 
     /**
      * Returns true if an image or project is open, false otherwise
@@ -149,32 +166,53 @@ public class GDCnnController {
     }
 
     /**
+     * Sets the progress indicator and label to show that the task is running
+     */
+    private void setProgressRunning(String taskName) {
+        tickIconImg.setVisible(false);
+        doneLabel.setVisible(false);
+        progressInd.setVisible(true);
+        progressLabel.setVisible(true);
+        progressLabel.setText(PROGRESS_MESSAGES.get(taskName));
+    }
+
+    /**
+     * Sets the progress indicator and label to show that the task is done
+     */
+    private void setProgressDone() {
+        progressInd.setVisible(false);
+        progressLabel.setVisible(false);
+        tickIconImg.setVisible(true);
+        doneLabel.setVisible(true);
+    }
+
+    /**
      * Puts the project images in the check list
      */
-	public void setImgsCheckListElements() {
-		ObservableList<String> imgsCheckListItems = FXCollections.observableArrayList();
+    public void setImgsCheckListElements() {
+        ObservableList<String> imgsCheckListItems = FXCollections.observableArrayList();
         Project<BufferedImage> project = qupath.getProject();
-		if (project != null) {
-			// Add all images in the project to the list
-        	List<ProjectImageEntry<BufferedImage>> imageEntryList = project.getImageList();
+        if (project != null) {
+            // Add all images in the project to the list
+            List<ProjectImageEntry<BufferedImage>> imageEntryList = project.getImageList();
 
-			for (ProjectImageEntry<BufferedImage> imageEntry : imageEntryList) {
-				String imageName = GeneralTools.stripExtension(imageEntry.getImageName());
-				imgsCheckListItems.add(imageName);
-			}
-		} else {
-			// Add the current image to the list
+            for (ProjectImageEntry<BufferedImage> imageEntry : imageEntryList) {
+                String imageName = GeneralTools.stripExtension(imageEntry.getImageName());
+                imgsCheckListItems.add(imageName);
+            }
+        } else {
+            // Add the current image to the list
             ImageData<BufferedImage> imageData = qupath.getImageData();
             if (imageData != null) {
-				String imageName = GeneralTools.stripExtension(imageData.getServer().getMetadata().getName());
-				imgsCheckListItems.add(imageName);
-			} else {
-				logger.error("No project or image is open");
-			}
-		}
+                String imageName = GeneralTools.stripExtension(imageData.getServer().getMetadata().getName());
+                imgsCheckListItems.add(imageName);
+            } else {
+                logger.error("No project or image is open");
+            }
+        }
 
-		imgsCheckList.setItems(imgsCheckListItems);
-	}
+        imgsCheckList.setItems(imgsCheckListItems);
+    }
 
     /**
      * Submits a task to the thread pool to run in the background
@@ -182,35 +220,35 @@ public class GDCnnController {
      * @param task
      */
     private void submitTask(Task<?> task) {
+        task.setOnRunning(e -> {
+            logger.info("Task running");
+            setProgressRunning(task.getClass().getSimpleName());
+        });
         task.setOnSucceeded(e -> {
-            logger.info("Task succeeded");
-            Dialogs.showInfoNotification("Task succeeded", task.getClass().getSimpleName() + " succeeded");
+            if (task instanceof DetectionTask || task instanceof ClassificationTask) {
+                // If there is an image selected, select an object from the
+                // hierarchy and deselect it to refresh the viewer
+                ImageData<BufferedImage> currentImageData = qupath.getViewer().getImageData();
+                if (currentImageData != null) {
+                    PathObjectHierarchy hierarchy = currentImageData.getHierarchy();
+                    Collection<PathObject> objects = hierarchy.getAnnotationObjects();
+                    if (!objects.isEmpty()) {
+                        PathObject object = objects.iterator().next();
+                        PathObjectSelectionModel selectionModel = hierarchy.getSelectionModel();
+                        selectionModel.setSelectedObject(object);
+                        selectionModel.clearSelection();
+                    }
+                }
+                logger.info("Task succeeded");
+                Dialogs.showInfoNotification("Task succeeded", task.getClass().getSimpleName() + " succeeded");
+                setProgressDone();
+            }
         });
         task.setOnFailed(e -> {
             logger.error("Task failed", e.getSource().getException());
             Dialogs.showErrorMessage("Task failed", e.getSource().getException());
         });
         pool.submit(task);
-        task.stateProperty().addListener((obs, oldState, newState) -> taskStateChange(task, newState));
-    }
-
-    /**
-     * Handles the state change of a task
-     * 
-     * @param task
-     * @param newState
-     */
-    private void taskStateChange(Task<?> task, Task.State newState) {
-        if (newState == Task.State.SUCCEEDED) {
-            logger.info("Task {} succeeded", task);
-            completedTasks++;
-        } else if (newState == Task.State.FAILED) {
-            logger.error("Task {} failed", task);
-            completedTasks++;
-        } else if (newState == Task.State.CANCELLED) {
-            logger.info("Task {} cancelled", task);
-            completedTasks++;
-        }
     }
 
     /**
@@ -262,13 +300,6 @@ public class GDCnnController {
      */
     public void classifyGlomeruli() throws IOException {
         submitTask(new ClassificationTask(qupath, "swin_transformer"));
-        // If dealing with a project, remove the image data from the viewer
-        // to refresh the viewer after the detection
-        Project<BufferedImage> project = qupath.getProject();
-        ImageData<BufferedImage> currentImageData = qupath.getViewer().getImageData();
-        if (project != null && currentImageData != null) {
-            qupath.getViewer().setImageData(null);
-        }
     }
-	
+
 }
